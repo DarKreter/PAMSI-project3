acba91815580596959b0cd8b0fd168378f4ede48
DarKreter <darkreter@gmail.com>
2022-06-10 21:39:41 +0200

    Add figures to board

--- Board/inc/board.hpp
+++ Board/inc/board.hpp
@@ -1,17 +1,28 @@
 #ifndef BOARD_PAMSI_PROJECT_DEFINE_H
 #define BOARD_PAMSI_PROJECT_DEFINE_H
 
+#include <Figure.hpp>
 #include <SFML/Graphics.hpp>
 #include <Tile.hpp>
-#include <array>
+#include <memory>
+#include <vector>
+
+#define fillColor_1 sf::Color::Red
+#define fillColor_2 sf::Color::White
+#define outlineColor sf::Color::Black
 
 namespace pamsi {
 
 class Board_t : public sf::Drawable {
-#define fillColor_1 sf::Color::Red
-#define fillColor_2 sf::Color::White
-#define outlineColor sf::Color::Black
     std::vector<std::vector<pamsi::Tile_t>> tiles;
+    std::vector<std::shared_ptr<Figure_t>> whiteFigures;
+    std::vector<std::shared_ptr<Figure_t>> blackFigures;
+
+    sf::Texture whitePawn, blackPawn;
+
+    void SetUpTextures();
+    void SetUpFigures(float windowsSize, float borderWidth);
+    void SetUpTiles(float windowsSize, float borderWidth);
 
 public:
     Board_t(float windowsSize, float borderWidth);
--- Board/src/board.cpp
+++ Board/src/board.cpp
@@ -1,4 +1,5 @@
 #include "board.hpp"
+#include <iostream>
 
 namespace pamsi {
 void Board_t::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -6,15 +7,65 @@ void Board_t::draw(sf::RenderTarget& target, sf::RenderStates states) const
     for(auto& a : tiles)
         for(auto& tile : a)
             target.draw(tile, states);
+
+    for(auto& figure : whiteFigures)
+
+        target.draw(*figure, states);
+
+    for(auto& figure : blackFigures)
+        target.draw(*figure, states);
 }
 
 Board_t::Board_t(float windowsSize, float borderWidth)
 {
-    float tileLength = (windowsSize / 8.f);
+    SetUpTiles(windowsSize, borderWidth);
+    SetUpTextures();
+    SetUpFigures(windowsSize, borderWidth);
+}
 
-    for(size_t x = 0; x < windowsSize; x += tileLength) {
+void Board_t::SetUpFigures(float windowsSize, float borderWidth)
+{
+    float tileLength = (windowsSize / 8.f);
+    float figureRadius = (tileLength - borderWidth) * 0.45f;
+
+    auto calcPos = [tileLength, borderWidth](auto x, auto y) -> sf::Vector2f {
+        return sf::Vector2f(x * tileLength + borderWidth / 2.f +
+                                (tileLength - borderWidth) * 0.05f,
+                            y * tileLength + borderWidth / 2.f +
+                                (tileLength - borderWidth) * 0.05f);
+    };
+
+    for(size_t x = 0; x < 8; x++) {
+        for(size_t y = 5; y < 8; y++) {
+            auto temp = std::make_shared<Figure_t>(figureRadius);
+
+            if(x % 2 != y % 2) {
+                temp->SetTexture(whitePawn);
+                temp->SetPosition(calcPos(x, y));
+                whiteFigures.emplace_back(temp);
+            }
+        }
+    }
+
+    for(size_t x = 0; x < 8; x++) {
+        for(size_t y = 0; y < 3; y++) {
+            auto temp = std::make_shared<Figure_t>(figureRadius);
+
+            if(x % 2 != y % 2) {
+                temp->SetTexture(blackPawn);
+                temp->SetPosition(calcPos(x, y));
+                blackFigures.emplace_back(temp);
+            }
+        }
+    }
+}
+void Board_t::SetUpTiles(float windowsSize, float borderWidth)
+{
+    float tileLength = (windowsSize / 8.f);
+    // Setup tiles
+    for(size_t x = 0; x < 8; x++) {
         std::vector<pamsi::Tile_t> tempVector;
-        for(size_t y = 0; y < windowsSize; y += tileLength) {
+        for(size_t y = 0; y < 8; y++) {
 
             pamsi::Tile_t temp(sf::Vector2f(tileLength, tileLength));
 
@@ -25,11 +76,24 @@ Board_t::Board_t(float windowsSize, float borderWidth)
 
             temp.SetBorderWidth(borderWidth);
             temp.SetOutlineColor(outlineColor);
-            temp.SetPosition(sf::Vector2f(x, y));
+            temp.SetPosition(sf::Vector2f(x * tileLength, y * tileLength));
 
             tempVector.emplace_back(temp);
         }
         tiles.emplace_back(tempVector);
     }
 }
+
+void Board_t::SetUpTextures()
+{
+    if(!whitePawn.loadFromFile("whitePawn.jpg"))
+        throw std::runtime_error("\"whitePawn.jpg not found!\"");
+
+    if(!blackPawn.loadFromFile("blackPawn.jpg"))
+        throw std::runtime_error("\"blackPawn.jpg not found!\"");
+
+    whitePawn.setSmooth(true);
+    blackPawn.setSmooth(true);
+}
+
 } // namespace pamsi
