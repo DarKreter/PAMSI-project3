223481abfee1ca2b5a6a5347c90101db8890e378
DarKreter <darkreter@gmail.com>
2022-06-10 21:35:44 +0200

    Add Pawn

---
+++ Pawn/inc/pawn.hpp
@@ -0,0 +1,16 @@
+#ifndef PAWN_PAMSI_PROJECT_DEFINE_H
+#define PAWN_PAMSI_PROJECT_DEFINE_H
+
+#include "figure.hpp"
+
+namespace pamsi {
+
+class Pawn_t : public pamsi::Figure_t {
+
+public:
+    Pawn_t(float radius) : Figure_t(radius){};
+};
+
+} // namespace pamsi
+
+#endif // PAWN_PAMSI_PROJECT_DEFINE_H
---
+++ Pawn/src/pawn.cpp
@@ -0,0 +1,5 @@
+#include "pawn.hpp"
+
+namespace pamsi {
+
+} // namespace pamsi
